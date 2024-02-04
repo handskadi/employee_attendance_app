52c461e326c302ddb7addbe787d3b7086375a31d
Mohamed KADI <handskadi@gmail.com>
2024-02-04 23:01:42 +0000

    Botstrap

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask;
+from flask import render_template;
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h2>Welcome to MKweb!</h2>"
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
  
