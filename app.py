4e00114c44da3354f9a707e5a4c4fef19dcd96d0
Mohamed KADI <handskadi@gmail.com>
2024-02-05 01:21:37 +0000

    Database creation and linking

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask;
-from flask import render_template;
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:satabase.db'
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
- 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
