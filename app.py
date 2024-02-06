403e0d086084f9365d579586ddb1a94a7e4bab4a
Mohamed KADI <handskadi@gmail.com>
2024-02-06 00:37:57 +0000

    Database Intgration

--- app.py
+++ app.py
@@ -1,17 +1,52 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
 
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:satabase.db'
 
-@app.route("/")
+# Create table for Database
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+                username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                    'That username already exists. Please choose a different one.')
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+        InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+    password = PasswordField(validators=[
+        InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+    submit = SubmitField('Login')
+
+@app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route("/about")
+@app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,5 @@
+# config.py
+
+SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost/eap'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SECRET_KEY = 'mysecretkey'
