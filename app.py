b855127c6f47587d34233d802a16cafdee884165
Mohamed KADI <handskadi@gmail.com>
2024-02-15 02:44:48 +0100

    Add .env to .gitignore

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, request, session, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
@@ -7,12 +9,15 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-# MySQL Configuration:
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'mkweb'
-app.config['MYSQL_PASSWORD'] = 'kadi@2023'
-app.config['MYSQL_DB'] = 'eap'
-app.secret_key = 'my_secret_key'
+# Load environment variables from .env file
+load_dotenv()
+
+# MySQL Configuration
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.secret_key = os.getenv('SECRET_KEY')
 
 mysql = MySQL(app)
 
