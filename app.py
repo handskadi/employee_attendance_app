4410dc864c865380ee6ae33cd3dd6870f7b7ff38
Mohamed KADI <handskadi@gmail.com>
2024-02-19 02:53:52 +0100

    Photo Upload completed

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from wtforms import StringField, PasswordField, SubmitField, SelectField, DateFi
 from wtforms.validators import DataRequired, Email, ValidationError
 import bcrypt
 from flask_mysqldb import MySQL
-
+from werkzeug.utils import secure_filename
+from flask_wtf.file import FileField, FileAllowed
+from flask import current_app
 
 app = Flask(__name__)
 
@@ -19,8 +21,14 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.secret_key = os.getenv('SECRET_KEY')
-
 mysql = MySQL(app)
+
+# Define the upload folder within the static directory
+UPLOAD_FOLDER = os.path.join('static', 'uploads')
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 class CreateForm(FlaskForm):
     project_name = StringField("Project Name", validators=[DataRequired()])
@@ -58,7 +66,8 @@ class RegisterForm(FlaskForm):
     password = PasswordField("Temporary Password", validators=[DataRequired()])
     hire_date = DateField('Hire Date', format='%Y-%m-%d', validators=[DataRequired()])
     end_employment = DateField('End of Employment', format='%Y-%m-%d', validators=[DataRequired()])
-    # retrive  all projects
+
+    # retrive all projects
     with app.app_context():
         cursor = mysql.connection.cursor()
         cursor.execute("SELECT * FROM project WHERE is_active='1'")
@@ -66,6 +75,7 @@ class RegisterForm(FlaskForm):
         cursor.close()
         project_choices =  [(str(project[0]), project[1]) for project in projects]
         project = SelectField('Assign Project', choices=project_choices, validators=[DataRequired()])
+    
     # retive all managers
     with app.app_context():
         cursor = mysql.connection.cursor()
@@ -74,8 +84,14 @@ class RegisterForm(FlaskForm):
         cursor.close()
         manager_choices =  [(str(manager[0]), manager[6] +' '+ manager[7]) for manager in managers]
         manager = SelectField('Assign Manager', choices=manager_choices, validators=[DataRequired()])
-     
+    
+    image = FileField("Upload Image", validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
     submit = SubmitField("Add Employee")
+
+    # Add UPLOAD_FOLDER to the form instance
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+        self.upload_folder = current_app.config['UPLOAD_FOLDER']
 
     def validate_email(self, field):
         cursor = mysql.connection.cursor()
@@ -84,6 +100,15 @@ class RegisterForm(FlaskForm):
         cursor.close()
         if user:
             raise ValidationError('Email Already Taken')
+    
+    def save_image(self, image):
+        if image:
+            filename = secure_filename(image.filename)
+            os.makedirs(self.upload_folder, exist_ok=True)  # Create the directory if it doesn't exist
+            image_path = os.path.join(self.upload_folder, filename)
+            image.save(image_path)
+            return filename
+        return None
 
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
@@ -102,6 +127,7 @@ def register():
     user = cursor.fetchone()
     if user and 'user_id' in session and session['user_id'] == 1:
         form = RegisterForm()
+        form.upload_folder = app.config['UPLOAD_FOLDER']
         if form.validate_on_submit():
             first_name = form.first_name.data
             last_name = form.last_name.data
@@ -113,6 +139,7 @@ def register():
             project = form.project.data
             manager = form.manager.data  
             password = form.password.data 
+            image = form.image.data
 
             hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
@@ -123,8 +150,12 @@ def register():
             cursor.execute("SELECT user_id FROM user where email=%s", (form.email.data,))
             # Store data into user employee table
             user_id_employee = cursor.fetchone()[0]
-            cursor.execute("INSERT INTO employee  (user_id, hire_date, end_employment, manager_id, project_id, firstname, lastname, role) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", 
-            (user_id_employee, hire_date, end_employment, manager, project, first_name, last_name, role))
+
+            # Save the image and get the filename
+            image_filename = form.save_image(image)
+
+            cursor.execute("INSERT INTO employee  (user_id, hire_date, end_employment, manager_id, project_id, firstname, lastname, role, image_filename) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+                           (user_id_employee, hire_date, end_employment, manager, project, first_name, last_name, role, image_filename))
             mysql.connection.commit()
             cursor.close()
 
@@ -132,6 +163,7 @@ def register():
 
         return render_template('register.html', form=form)
     return redirect(url_for('login'))
+
 
 @app.route('/login', methods=['GET','POST'])
 def login():
