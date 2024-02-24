9aea7d6739d969861013f9f0b40d437dc24afb59
Mohamed KADI <handskadi@gmail.com>
2024-02-24 22:12:52 +0100

    Employe Edit: Done

--- app.py
+++ app.py
@@ -302,6 +302,120 @@ def logout():
     flash("You have been logged out successfully.")
     return redirect(url_for('login'))
 
+class EditEmployeeForm(FlaskForm):
+    first_name = StringField("First Name", validators=[DataRequired()])
+    last_name = StringField("Last Name", validators=[DataRequired()])
+    # email = StringField("Email", validators=[DataRequired(), Email()])
+    role = SelectField('Select Role', choices=[('general_manager', 'General Manager'), ('project_manager', 'Project Manager'), ('employee', 'Employee') ], validators=[DataRequired()])
+    # username = StringField("Username", validators=[DataRequired()])
+    # password = PasswordField("Temporary Password", validators=[DataRequired()])
+    hire_date = DateField('Hire Date', format='%Y-%m-%d', validators=[DataRequired()])
+    end_employment = DateField('End of Employment', format='%Y-%m-%d', validators=[DataRequired()])
+
+    # retrive all projects
+    with app.app_context():
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM project WHERE is_active='1'")
+        projects = cursor.fetchall()  
+        cursor.close()
+        project_choices =  [(str(project[0]), project[1]) for project in projects]
+        project = SelectField('Assign Project', choices=project_choices, validators=[DataRequired()])
+    
+    # retive all managers
+    with app.app_context():
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM employee WHERE role='project_manager'")
+        managers = cursor.fetchall()  
+        cursor.close()
+        manager_choices =  [(str(manager[0]), manager[6] +' '+ manager[7]) for manager in managers]
+        manager = SelectField('Assign Manager', choices=manager_choices, validators=[DataRequired()])
+    image = FileField("Upload Image", validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+    submit = SubmitField("Save Edit")
+
+    # Add UPLOAD_FOLDER to the form instance
+    def __init__(self, *args, **kwargs):
+        super(EditEmployeeForm, self).__init__(*args, **kwargs)
+        self.upload_folder = current_app.config['UPLOAD_FOLDER']
+
+    def save_image(self, image):
+        if image:
+            filename = secure_filename(image.filename)
+            os.makedirs(self.upload_folder, exist_ok=True)  # Create the directory if it doesn't exist
+            image_path = os.path.join(self.upload_folder, filename)
+            image.save(image_path)
+            return filename
+        return None
+
+@app.route('/edit_employee/<int:employee_id>', methods=['GET', 'POST'])
+def edit_employee(employee_id):
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM employee WHERE employee_id = %s", (employee_id,))
+    employee = cursor.fetchone()
+    cursor.close()
+
+    if not employee:
+        flash("Employee not found", "error")
+        return redirect('page_not_found')
+
+    form = EditEmployeeForm()
+    form.upload_folder = app.config['UPLOAD_FOLDER']
+    if request.method == 'GET': 
+        form = EditEmployeeForm(
+            obj=employee, 
+            first_name=employee[6], 
+            last_name=employee[7], 
+            role=employee[8], 
+            hire_date=employee[2], 
+            end_employment=employee[3], 
+            project=employee[5],
+            manager=employee[4],
+            image=employee[9])  # Populate the form with existing data
+        
+    if form.validate_on_submit():
+        print("Hello")
+        # Update employee details based on form data
+        # Note: You may need to customize this based on your database schema
+        uploaded_image = form.image.data
+        if uploaded_image == None and employee[9] != 'default.png':
+            uploaded_image = employee[9]
+        elif uploaded_image == None and employee[9] == 'default.png':
+            uploaded_image = 'default.png'
+        else: 
+            uploaded_image = form.image.data.filename
+            print(uploaded_image)
+            
+        # Save the image and get the filename
+        form.save_image(form.image.data)
+
+        print(uploaded_image)
+        updated_data = {
+            'first_name': form.first_name.data,
+            'last_name': form.last_name.data,
+            'role': form.role.data,
+            'hire_date': form.hire_date.data,
+            'end_employment': form.end_employment.data,
+            'project': form.project.data,
+            'manager': form.manager.data,
+            'image': uploaded_image
+        }
+
+        # Update the database with the new data
+        cursor = mysql.connection.cursor()
+        cursor.execute("""
+            UPDATE employee
+            SET firstname = %(first_name)s, lastname = %(last_name)s,
+                role = %(role)s,  hire_date = %(hire_date)s,
+                end_employment = %(end_employment)s, project_id= %(project)s, manager_id = %(manager)s, image_filename = %(image)s
+            WHERE employee_id = %(employee_id)s
+        """, {**updated_data, 'employee_id': employee_id})
+        mysql.connection.commit()
+        cursor.close()
+
+        flash("Employee details updated successfully")
+        # return redirect(url_for('dashboard'))
+        return redirect(url_for('edit_employee', employee_id=employee_id))
+
+    return render_template('edit_employee.html', form=form, employee_id=employee_id)
 # Custom error handler for 404 errors
 @app.errorhandler(404)
 def page_not_found(error):
